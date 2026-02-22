25e17cb411f644c6c36689a5bf43d8651e9207c0
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-02-22 11:17:37 +0100

    nicer 'is not a constant'
    f nicer 'is not a constant'

--- turbopack/crates/turbopack-ecmascript/src/analyzer/graph.rs
+++ turbopack/crates/turbopack-ecmascript/src/analyzer/graph.rs
@@ -367,7 +367,7 @@ pub struct VarGraph {
     // import -> immediate usage (top level decl)
     pub import_usages: FxHashMap<usize, DeclUsage>,
     // export name -> top level decl
-    pub exports: FxHashMap<Atom, Id>,
+    pub exports: FxHashMap<Atom, (Id, Span)>,
 }
 
 impl VarGraph {
@@ -2983,17 +2983,19 @@ impl VisitAstPath for Analyzer<'_> {
             Decl::Class(node) => {
                 self.data
                     .exports
-                    .insert(node.ident.sym.clone(), node.ident.to_id());
+                    .insert(node.ident.sym.clone(), (node.ident.to_id(), node.span()));
             }
             Decl::Fn(node) => {
                 self.data
                     .exports
-                    .insert(node.ident.sym.clone(), node.ident.to_id());
+                    .insert(node.ident.sym.clone(), (node.ident.to_id(), node.span()));
             }
             Decl::Var(node) => {
                 for VarDeclarator { name, .. } in &node.decls {
                     for_each_ident_in_pat(name, &mut |name, ctxt| {
-                        self.data.exports.insert(name.clone(), (name.clone(), ctxt));
+                        self.data
+                            .exports
+                            .insert(name.clone(), ((name.clone(), ctxt), node.span()));
                     });
                 }
             }
@@ -3016,7 +3018,7 @@ impl VisitAstPath for Analyzer<'_> {
         self.data.exports.insert(
             export_name,
             match &node.orig {
-                ModuleExportName::Ident(ident) => ident.to_id(),
+                ModuleExportName::Ident(ident) => (ident.to_id(), node.span()),
                 ModuleExportName::Str(_) => unreachable!("exporting a string should be impossible"),
             },
         );
--- turbopack/crates/turbopack-ecmascript/src/references/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/mod.rs
@@ -792,7 +792,7 @@ async fn analyze_ecmascript_module_internal(
                         var_graph
                             .exports
                             .iter()
-                            .filter(|(_, id)| visited.contains(*id))
+                            .filter(|(_, (id, _))| visited.contains(id))
                             .map(|(exported, _)| exported.as_str().into())
                             .collect(),
                     )
