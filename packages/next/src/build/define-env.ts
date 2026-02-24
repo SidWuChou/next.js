59f561b56053a39ed934a7693106ee936bb0e420
Tim Neutkens <tim@timneutkens.nl>
2026-02-24 13:22:51 +0100

    feat(node-streams): add config flag, define-env, and env precedence test

--- packages/next/src/build/define-env.ts
+++ packages/next/src/build/define-env.ts
@@ -121,6 +121,7 @@ export function getDefineEnv({
 
   const isCacheComponentsEnabled = !!config.cacheComponents
   const isUseCacheEnabled = !!config.experimental.useCache
+  const isUseNodeStreamsEnabled = !!config.experimental.useNodeStreams
 
   const defineEnv: DefineEnv = {
     // internal field to identify the plugin config
@@ -172,6 +173,9 @@ export function getDefineEnv({
     'process.env.__NEXT_INSTANT_NAV_TOGGLE':
       !!config.experimental.instantNavigationDevToolsToggle,
     'process.env.__NEXT_USE_CACHE': isUseCacheEnabled,
+    'process.env.__NEXT_USE_NODE_STREAMS': isEdgeServer
+      ? false
+      : isUseNodeStreamsEnabled,
 
     'process.env.NEXT_IMMUTABLE_ASSET_TOKEN':
       config.experimental.immutableAssetToken || '',
