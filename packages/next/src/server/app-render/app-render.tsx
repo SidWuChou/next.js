f9615f48ec8900cf2fdcaf291921dc447c6dff7f
Tim Neutkens <tim@timneutkens.nl>
2026-03-03 15:48:13 +0100

    Fix bugs

--- packages/next/src/server/app-render/app-render.tsx
+++ packages/next/src/server/app-render/app-render.tsx
@@ -3482,14 +3482,21 @@ async function renderWithRestartOnCacheMissInDev(
       startTime = performance.now() + performance.timeOrigin
 
       const streamPair = teeStream(
-        renderToFlightStream(ComponentMod, initialRscPayload, clientModules, {
-          onError,
-          environmentName,
-          startTime,
-          filterStackFrame,
-          debugChannel: debugChannel?.serverSide,
-          signal: initialReactController.signal,
-        })
+        workUnitAsyncStorage.run(
+          requestStore,
+          renderToFlightStream,
+          ComponentMod,
+          initialRscPayload,
+          clientModules,
+          {
+            onError,
+            environmentName,
+            startTime,
+            filterStackFrame,
+            debugChannel: debugChannel?.serverSide,
+            signal: initialReactController.signal,
+          }
+        )
       )
 
       // If we abort the render, we want to reject the stage-dependent promises as well.
@@ -3629,13 +3636,20 @@ async function renderWithRestartOnCacheMissInDev(
       startTime = performance.now() + performance.timeOrigin
 
       const streamPair = teeStream(
-        renderToFlightStream(ComponentMod, finalRscPayload, clientModules, {
-          onError,
-          environmentName,
-          startTime,
-          filterStackFrame,
-          debugChannel: debugChannel?.serverSide,
-        })
+        workUnitAsyncStorage.run(
+          requestStore,
+          renderToFlightStream,
+          ComponentMod,
+          finalRscPayload,
+          clientModules,
+          {
+            onError,
+            environmentName,
+            startTime,
+            filterStackFrame,
+            debugChannel: debugChannel?.serverSide,
+          }
+        )
       )
 
       return {
